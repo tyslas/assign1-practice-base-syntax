8c8b05b74c25562064189c1db218d9c0e0038e96
Tito Yslas <roberto.yslas@gmail.com>
2017-11-27 10:54:42 -0700

    added a method to manipulate the state (i.e. an event handler)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import UserOutput from './userInOut/UserOutput'
 
 class App extends Component {
   state = {
     users: [
-      {username: 'Kwon'},
-      {username: 'Chunks'},
-      {username: 'Neutral'}
+      {
+        username: 'Kwon',
+        food: 'KBBQ'
+      }, {
+        username: 'Chunks',
+        food: 'Viz-Danko'
+      }, {
+        username: 'Neutral',
+        food: 'Matzah'
+      }
     ]
   }
+
+  foodChangedHandler = (e => {
+    this.setState({
+      users: [
+        {
+          username: 'Kwon',
+          food: e.target.value
+        }, {
+          username: 'Chunks',
+          food: e.target.value
+        }, {
+          username: 'Neutral',
+          food: e.target.value
+        }
+      ]
+    })
+  })
+
   render() {
-    return (
-      <div className="App">
-        <div className="appHeader">
-          <h1>Learning React on Udemy:</h1>
-          <h3>Assignment 1: Time to Practice - The Base Syntax</h3>
-        </div>
-        <UserOutput
-          username={this.state.users[0].username}
-        />
-        <UserOutput
-          username={this.state.users[1].username}
-        />
-        <UserOutput
-          username={this.state.users[2].username}
-        />
+    return (<div className="App">
+      <div className="appHeader">
+        <h1>Learning React on Udemy:</h1>
+        <h3>Assignment 1: Time to Practice - The Base Syntax</h3>
       </div>
-    );
+      <UserOutput username={this.state.users[0].username} />
+      <UserOutput username={this.state.users[1].username} />
+      <UserOutput username={this.state.users[2].username} />
+    </div>);
   }
 }
 
