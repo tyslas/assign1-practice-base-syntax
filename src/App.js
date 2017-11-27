d9ad8551213595d2794bb8ed1041ae6d8c93bffe
Tito Yslas <roberto.yslas@gmail.com>
2017-11-27 09:52:01 -0700

    Output multiple UserOutput components in the App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserOutput from './userInOut/UserOutput'
 
 class App extends Component {
   render() {
@@ -10,7 +10,9 @@ class App extends Component {
           <h1>Learning React on Udemy:</h1>
           <h3>Assignment 1: Time to Practice - The Base Syntax</h3>
         </div>
-        
+        <UserOutput />
+        <UserOutput />
+        <UserOutput />
       </div>
     );
   }
--- src/userInOut/UserOutput.js
+++ src/userInOut/UserOutput.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const userOutput = (props) => {
   return (
-    <p>Your final meal shoule be {props.food}!!</p>
-    <p>If you don't already have a onesie or snuggie with a picture of {props.food}, then I recommend that you get one!</p>
+    <div>
+      <p>Your final meal shoule be {props.food}!!</p>
+      <p>If you don't already have a onesie or snuggie with a picture of {props.food}, then I recommend that you get one!</p>
+    </div>
   )
 }
+
+export default userOutput;
